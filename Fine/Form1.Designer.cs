b1e79728c5d7e24958d2460d751167c961b35cc3
Беляев Валерий Николаевич <10150320@college.local>
2019-04-20 15:08:42 +0300

    ok

---
+++ Fine/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Fine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.avtoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fineDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fineDataSet = new Fine.fineDataSet();
+            this.avtoBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.avtoTableAdapter = new Fine.fineDataSetTableAdapters.AvtoTableAdapter();
+            this.avtoBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fineDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fineDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(446, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(340, 28);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Номер машины";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Дата";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Время";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Вид нарушения";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Размер штрафа";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(128, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(95, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(229, 28);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(95, 20);
+            this.dateTimePicker2.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Оформить штраф";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.avtoBindingSource2, "Id_avto", true));
+            this.comboBox1.DataSource = this.avtoBindingSource1;
+            this.comboBox1.DisplayMember = "Nomer";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.ValueMember = "Id_avto";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // avtoBindingSource
+            // 
+            this.avtoBindingSource.DataMember = "Avto";
+            // 
+            // fineDataSet
+            // 
+            this.fineDataSet.DataSetName = "fineDataSet";
+            this.fineDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // avtoBindingSource1
+            // 
+            this.avtoBindingSource1.DataMember = "Avto";
+            this.avtoBindingSource1.DataSource = this.fineDataSet;
+            // 
+            // avtoTableAdapter
+            // 
+            this.avtoTableAdapter.ClearBeforeFill = true;
+            // 
+            // avtoBindingSource2
+            // 
+            this.avtoBindingSource2.DataMember = "Avto";
+            this.avtoBindingSource2.DataSource = this.fineDataSet;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fineDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fineDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avtoBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource fineDataSetBindingSource;
+        private System.Windows.Forms.BindingSource avtoBindingSource;
+        private fineDataSet fineDataSet;
+        private System.Windows.Forms.BindingSource avtoBindingSource1;
+        private fineDataSetTableAdapters.AvtoTableAdapter avtoTableAdapter;
+        private System.Windows.Forms.BindingSource avtoBindingSource2;
+    }
+}
+
---
+++ Fine/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fine
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Text != "" && dateTimePicker2.Text != "" && dateTimePicker2.Text != "" && textBox5.Text != "" && textBox3.Text != "" && comboBox1.SelectedValue != "")
+            {
+                SqlCommand command1 = new SqlCommand("INSERT INTO  [fine] (data_fine,type_nar,Raz_fine,Id_avto, Time) VALUES(@data_fine,@type_nar,@Raz_fine,@Id_avto,@Time)", sqlConnection);
+                command1.Parameters.AddWithValue("data_fine", dateTimePicker1.Value);
+                command1.Parameters.AddWithValue("Time", dateTimePicker2.Value);
+                command1.Parameters.AddWithValue("type_nar", textBox5.Text);
+                command1.Parameters.AddWithValue("Raz_fine", textBox3.Text);
+                command1.Parameters.AddWithValue("Id_avto", comboBox1.SelectedValue);
+                await command1.ExecuteNonQueryAsync();
+                MessageBox.Show("Вы оформили штраф",
+      "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Вы не заполнели один из полей",
+        "Предуприждение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "fineDataSet.Avto". При необходимости она может быть перемещена или удалена.
+            this.avtoTableAdapter.Fill(this.fineDataSet.Avto);
+            String connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\10150320\source\repos\Fine\Fine\fine.mdf;Integrated Security=True;Connect Timeout=30";
+            sqlConnection = new SqlConnection(connectionString);
+            await sqlConnection.OpenAsync();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
